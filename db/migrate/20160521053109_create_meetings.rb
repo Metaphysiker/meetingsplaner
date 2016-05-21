22109763d80e57412e8ef752ff0672cfd04740c6
Metaphysiker <s.raess@me.com>
2016-05-21 07:39:09 +0200

    first commit

---
+++ db/migrate/20160521053109_create_meetings.rb
@@ -0,0 +1,10 @@
+class CreateMeetings < ActiveRecord::Migration
+  def change
+    create_table :meetings do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
